c558b3343dcf0a106d14cbc9b97a8679564efb6a
Smyy <sumeyyekazanci13@gmail.com>
2022-06-04 00:04:03 +0300

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Sümeyye Kazancı on 4.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
